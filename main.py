d3d03012f5c4864c2ef585aa355e5e952ba623bf
sajeevanh2r <sajeevan2001625@gmail.com>
2025-10-16 16:58:50 +0530

    completed the data ingestion

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), "src"))
+
+
+from CNNClassifier import logger
+from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
