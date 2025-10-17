cce18db9570536a8786a7f3cf98e112c07f2f876
sajeevanh2r <sajeevan2001625@gmail.com>
2025-10-17 14:54:55 +0530

    Finished the Model Evaluation Process

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from CNNClassifier import logger
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from CNNClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CNNClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 
@@ -42,4 +43,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Evaluation"
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 from CNNClassifier.constants import *
 from CNNClassifier.utils.common import read_yaml, create_directories
-from CNNClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+from CNNClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig)
 import os
+from pathlib import Path
 
 
 class ConfigurationManager:
@@ -89,3 +90,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/dataset"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config   
